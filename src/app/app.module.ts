e3a760d7f8099410804ed0df8d67e29cc7267937
joan-tg <joantormogarcia@gmail.com>
2020-06-01 18:06:18 +0200

    Primer componente generado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ZapatillasComponent } from './zapatillas/zapatillas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ZapatillasComponent
   ],
   imports: [
     BrowserModule,
